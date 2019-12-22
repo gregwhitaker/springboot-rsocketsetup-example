46d4cd9c23548f8a8a592beb2adfc9a208987dba
gregwhitaker <gwhitake@gmail.com>
2019-12-21 23:57:35 -0800

    Doing work

---
+++ hello-service/src/main/java/example/service/hello/controller/HelloController.java
@@ -0,0 +1,11 @@
+package example.service.hello.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HelloController {
+    private static final Logger LOG = LoggerFactory.getLogger(HelloController.class);
+
+}
