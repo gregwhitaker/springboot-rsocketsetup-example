63a8625849a00ac4d3ec9be130360033678a3540
gregwhitaker <gwhitake@gmail.com>
2019-12-21 23:51:31 -0800

    Doing work

---
+++ hello-client/src/main/java/example/client/hello/HelloClientApplication.java
@@ -0,0 +1,26 @@
+package example.client.hello;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
+
+@SpringBootApplication
+public class HelloClientApplication {
+
+    public static void main(String... args) {
+        SpringApplication.run(HelloClientApplication.class);
+    }
+
+    /**
+     * Runs the application.
+     */
+    @Component
+    public class Runner implements CommandLineRunner {
+
+        @Override
+        public void run(String... args) throws Exception {
+
+        }
+    }
+}
