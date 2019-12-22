830c5c568a1d3d67355a0229615aeaaaae209b96
gregwhitaker <gwhitake@gmail.com>
2019-12-22 00:01:55 -0800

    Doing work

--- hello-service/src/main/java/example/service/hello/controller/HelloController.java
+++ hello-service/src/main/java/example/service/hello/controller/HelloController.java
@@ -2,10 +2,17 @@ package example.service.hello.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.messaging.rsocket.annotation.ConnectMapping;
 import org.springframework.stereotype.Controller;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Controller
 public class HelloController {
     private static final Logger LOG = LoggerFactory.getLogger(HelloController.class);
 
+    @ConnectMapping
+    public Mono<Void> setup() {
+        return Mono.empty();
+    }
 }
