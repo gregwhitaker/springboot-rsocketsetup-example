c14b24b62f983852e2d92b104ac36c4bd017c258
gregwhitaker <gwhitake@gmail.com>
2019-12-22 10:06:06 -0800

    Doing work

--- hello-service/src/main/java/example/service/hello/config/RSocketConfiguration.java
+++ hello-service/src/main/java/example/service/hello/config/RSocketConfiguration.java
@@ -1,8 +1,29 @@
 package example.service.hello.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.codec.cbor.Jackson2CborDecoder;
+import org.springframework.http.codec.json.Jackson2JsonDecoder;
+import org.springframework.messaging.rsocket.RSocketStrategies;
+import org.springframework.messaging.rsocket.annotation.support.RSocketMessageHandler;
 
 @Configuration
 public class RSocketConfiguration {
 
+    @Bean
+    public RSocketMessageHandler rsocketMessageHandler(RSocketStrategies rSocketStrategies) {
+        RSocketMessageHandler handler = new RSocketMessageHandler();
+        handler.setRSocketStrategies(rSocketStrategies);
+        return handler;
+    }
+
+    @Bean
+    public RSocketStrategies rsocketStrategies() {
+        return RSocketStrategies.builder()
+                .decoders(decoders -> {
+                    decoders.add(new Jackson2JsonDecoder());
+                    decoders.add(new Jackson2CborDecoder());
+                })
+                .build();
+    }
 }
